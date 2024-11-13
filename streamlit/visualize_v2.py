c72f06a48195b83ee643779e05ecce2c28bc97ae
sejongmin <jmoh7480@gmail.com>
2024-11-13 09:54:39 +0000

    [Refactor] : 코드 리펙토링

--- streamlit/visualize_v2.py
+++ streamlit/visualize_v2.py
@@ -5,25 +5,16 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
-COLOR = {
-    'finger': (120,203,228),
-    'Trapezoid': (145,42,177),
-    'Pisiform': (145,42,177),
-    'Radius': (210,71,77),
-    'Ulna': (210,71,77),
-    'wrist': (193,223,159)
-}
-
-def get_poly(points, label):
+def get_poly(points, label, color):
     if label.startswith('finger'):
         label = 'finger'
-    elif label not in COLOR:
+    elif label not in color:
         label = 'wrist'
         
     poly = patches.Polygon(
         points, 
         closed=True, 
-        facecolor=[ck/255 for ck in COLOR[label]], 
+        facecolor=[ck/255 for ck in color[label]], 
         edgecolor='black',
         alpha=0.7
     )
@@ -54,14 +45,14 @@ def show(info, images, labels, id, text='None', anno='All'):
         orin_label = annot['label'] 
         label = orin_label
         if anno == 'All':
-            poly = get_poly(points, label)
+            poly = get_poly(points, label, info['color'])
             ax_left.add_patch(poly)
             cx, cy = sum([p[0] for p in points]) / len(points), sum([p[1] for p in points]) / len(points)
             if text == 'Text':
                 ax_left.text(cx, cy, orin_label, fontsize=5, color='white')
         else:
             if label == anno:
-                poly = get_poly(points, label)
+                poly = get_poly(points, label, info['color'])
                 ax_left.add_patch(poly)
                 cx, cy = sum([p[0] for p in points]) / len(points), sum([p[1] for p in points]) / len(points)
                 if text == 'Text':
@@ -79,14 +70,14 @@ def show(info, images, labels, id, text='None', anno='All'):
         orin_label = annot['label'] 
         label = orin_label
         if anno == 'All':
-            poly = get_poly(points, label)
+            poly = get_poly(points, label, info['color'])
             ax_right.add_patch(poly)
             cx, cy = sum([p[0] for p in points]) / len(points), sum([p[1] for p in points]) / len(points)
             if text == 'Text':
                 ax_right.text(cx, cy, orin_label, fontsize=5, color='white')
         else:
             if label == anno:
-                poly = get_poly(points, label)
+                poly = get_poly(points, label, info['color'])
                 ax_right.add_patch(poly)
                 cx, cy = sum([p[0] for p in points]) / len(points), sum([p[1] for p in points]) / len(points)
                 if text == 'Text':

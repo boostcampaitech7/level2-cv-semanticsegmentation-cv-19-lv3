48e20be59bfbc1729952f3af8039c910eab2ca6d
parkjunil <june21r@gmail.com>
2024-11-21 15:36:08 +0900

    [Fix}:remake dataset config to have same image size at train and validation

--- mmsegmentation/configs/_custom_/_base_/xraydata2.py
+++ mmsegmentation/configs/_custom_/_base_/xraydata2.py
@@ -1,3 +1,5 @@
+IMAGE_SIZE = (1024, 1024)
+
 # dataset settings
 dataset_type = 'XRayDataset2'
 
@@ -5,46 +7,26 @@ dataset_type = 'XRayDataset2'
 train_pipeline = [
             dict(type='LoadImageFromFile'),
             dict(type='LoadXRayAnnotations'),
-            dict(type='Resize', scale=(1280, 1280)),
-            dict(type='RandomFlip', prob=0.5),
-            dict(type='RandomRotate', prob=0.5, degree=15),
-            dict(type='PhotoMetricDistortion'),
+            dict(type='Resize', scale=IMAGE_SIZE),
             dict(type='TransposeAnnotations'),
             dict(type='PackSegInputs')
         ]
 val_pipeline = [
             dict(type='LoadImageFromFile'),
+            dict(type='Resize', scale=IMAGE_SIZE),
             dict(type='LoadXRayAnnotations'),
             dict(type='TransposeAnnotations'),
             dict(type='PackSegInputs')
         ]
 test_pipeline = [
     dict(type='LoadImageFromFile'),
-    dict(type='Resize', scale=(2048, 2048), keep_ratio=True),
+    dict(type='Resize', scale=IMAGE_SIZE),
     dict(type='PackSegInputs')
 ]
 
-
-img_ratios = [0.5, 0.75, 1.0, 1.25, 1.5, 1.75]
-tta_pipeline = [
-    dict(type='LoadImageFromFile', backend_args=None),
-    dict(
-        type='TestTimeAug',
-        transforms=[
-            [
-                dict(type='Resize', scale_factor=r, keep_ratio=True)
-                for r in img_ratios
-            ],
-            [
-                dict(type='RandomFlip', prob=0., direction='horizontal'),
-                dict(type='RandomFlip', prob=1., direction='horizontal')
-            ], [dict(type='LoadAnnotations')], [dict(type='PackSegInputs')]
-        ])
-]
-
 train_dataloader = dict(
-    batch_size=2,
-    num_workers=1,
+    batch_size=1,
+    num_workers=2,
     # persistent_workers=True,
     sampler=dict(type='InfiniteSampler', shuffle=True),
     dataset=dict(
@@ -62,7 +44,7 @@ val_dataloader = dict(
 
 
 test_dataloader = dict(
-    batch_size=1,
+    batch_size=8,
     num_workers=4,
     persistent_workers=True,
     sampler=dict(type='DefaultSampler', shuffle=False),
@@ -76,3 +58,25 @@ test_dataloader = dict(
 
 val_evaluator = dict(type='DiceMetricForMultiLabel')
 test_evaluator = dict(type='SubmissionMetric')
+
+
+
+img_ratios = [0.5, 0.75, 1.0, 1.25, 1.5, 1.75]
+tta_pipeline = [
+    dict(type='LoadImageFromFile', backend_args=None),
+    dict(
+        type='TestTimeAug',
+        transforms=[
+            [
+                dict(type='Resize', scale_factor=r, keep_ratio=True)
+                for r in img_ratios
+            ],
+            [
+                dict(type='RandomFlip', prob=0., direction='horizontal'),
+                dict(type='RandomFlip', prob=1., direction='horizontal')
+            ], 
+            [
+                dict(type='PackSegInputs')
+            ]
+        ])
+]

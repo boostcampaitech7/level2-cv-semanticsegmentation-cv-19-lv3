ebca88054c31a998c414932010646a67c96bf284
Soy17 <thdudlee0617@naver.com>
2024-11-27 17:01:25 +0900

    [#11] UNet++ code update

--- streamlit/main.py
+++ streamlit/main.py
@@ -8,9 +8,8 @@ import pandas as pd
 
 
 info = {
-    'image_root': "../../data/{}/DCM",
-    'all_image_root': "../../data/test/DCM/all_pngs",
-    'label_root': "../../data/train/outputs_json",
+    'image_root': "/data/ephemeral/home/sy/level2-cv-semanticsegmentation-cv-19-lv3/data/test/DCM",
+    'label_root': "/data/ephemeral/home/sy/level2-cv-semanticsegmentation-cv-19-lv3/data/train/outputs_json",
     'classes': [
         'finger-1', 'finger-2', 'finger-3', 'finger-4', 'finger-5',
         'finger-6', 'finger-7', 'finger-8', 'finger-9', 'finger-10',
